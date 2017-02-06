2d3c22c34a07e75d795bae99e7d95110eb8c2a50
Erwan Mahe <erwan.mahe@cea.fr>
2017-02-06 13:03:44 +0100

    Bug 511757
    First draft
    effectively used for new project/example wizard
    
    Change-Id: Iff4f300d695e3bea5c929dcbbfbcd97e2c27ac1a
    Signed-off-by: Erwan Mahe <erwan.mahe@cea.fr>

---
+++ gui/org.eclipse.efm.ui.resources/src/org/eclipse/efm/ui/resources/UIfmlResources.java
@@ -0,0 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2016 CEA LIST.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Erwan Mahe (CEA LIST) erwan.mahe@cea.fr - Initial API and implementation
+ *******************************************************************************/
+package org.eclipse.efm.ui.resources;
+
+public class UIfmlResources {
+	
+	private static final String PROFILES_PATHMAP = "platform:/plugin/";
+	
+	public static final String FML_PROFILE_PATH = PROFILES_PATHMAP + "org.eclipse.efm.formalml/resources/profile/FormalML.profile.uml";
+	
+}
