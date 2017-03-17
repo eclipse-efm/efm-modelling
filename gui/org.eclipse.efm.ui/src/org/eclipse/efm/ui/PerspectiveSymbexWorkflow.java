0c8879083182edb9b1cd2df36334e959941c2f70
Arnault Lapitre <arnault.lapitre@cea.fr>
2017-03-17 10:00:45 +0100

    Bug 513796
    Initial commit of org.eclipse.efm.ui
    Change-Id: I6fb4cfbb6f5b94f04e7bce733ac5424d0137eaf4
    Signed-off-by: Arnault Lapitre <arnault.lapitre@cea.fr>

---
+++ gui/org.eclipse.efm.ui/src/org/eclipse/efm/ui/PerspectiveSymbexWorkflow.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2017 CEA LIST.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *  Arnault Lapitre (CEA LIST) arnault.lapitre@cea.fr
+ *   - Initial API and Implementation
+ *******************************************************************************/
+package org.eclipse.efm.ui;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class PerspectiveSymbexWorkflow implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
