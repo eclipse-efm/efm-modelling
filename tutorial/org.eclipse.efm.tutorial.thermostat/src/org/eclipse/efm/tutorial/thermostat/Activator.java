3bc96bd7d3f0e148ecc60e643cde419dba893a25
Arnault Lapitre <arnault.lapitre@cea.fr>
2017-07-31 14:47:40 +0200

    Bug 514634 General improvement
    [Refactor] Rename org.*.Thermostat to org.*.thermostat
    [Update] Adding plugins Intro an Tutorial to Feature's plugin
    
    Change-Id: I575e9ff3f02cf5357362f6a7ee641717a2550bdd
    Signed-off-by: Arnault Lapitre <arnault.lapitre@cea.fr>

--- tutorial/org.eclipse.efm.tutorial.thermostat/src/org/eclipse/efm/tutorial/thermostat/Activator.java
+++ tutorial/org.eclipse.efm.tutorial.thermostat/src/org/eclipse/efm/tutorial/thermostat/Activator.java
@@ -22,7 +22,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.efm.tutorial.Thermostat"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "org.eclipse.efm.tutorial.thermostat"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
